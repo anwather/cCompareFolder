9745f4c2d543e50377141ee853c764df22d811d1
Anthony Watherston <anwather@microsoft.com>
2016-07-19 17:07:58 +1200

    alter mod file

---
+++ Examples/Example.ps1
@@ -0,0 +1,26 @@
+﻿Configuration FolderTest
+    {
+
+    Import-DSCResource -ModuleName cCompareFolder
+
+    Node localhost
+
+        {
+        cFolder Folder
+            {
+            SourceFolder = "C:\Temp\Source"
+            DestinationFolder = "C:\Temp\Destination"
+            }
+        }
+
+
+    }
+
+#Prepare Module
+Copy-Item C:\Temp\cCompareFolder -Destination 'C:\Program Files\WindowsPowerShell\Modules' -Force
+
+Set-Location C:\temp
+
+FolderTest 
+
+Start-DSCConfiguration .\FolderTest -Wait -Verbose -Force
--- cCompareFolder.psm1
+++ cCompareFolder.psm1
@@ -1,10 +1,12 @@
 ﻿Function Compare-Folder
     {
     [CmdletBinding()]
-    Param($SourceFolder,$DestinationFolder)
+    Param($SourceFolder,$DestinationFolder,[switch]$Test)
 
     $source = Get-ChildItem $SourceFolder -Recurse
     $destination = Get-ChildItem $DestinationFolder
+
+
     
     try
         {
@@ -21,13 +23,16 @@
             $comp = $null
         }
 
+if ($Test)
+    {
     if ($null -ne $comp)
         {
-        switch($comp)
+        <#switch($comp)
             {
                 {$_.SideIndicator -eq "<="} {Write-Verbose "Source: $($_.InputObject) does not exist in $DestinationFolder";Copy-Item "$SourceFolder\$($_.InputObject)" -Destination $destinationFolder -Verbose}
                 {$_.SideIndicator -eq "=>"} {Write-Verbose "Destination: $($_.InputObject) does not exist in $SourceFolder";Remove-Item "$DestinationFolder\$($_.InputObject)" -Force -Verbose}
-            }
+            }#>
+            Write-Verbose "Differences Found"
         return $false
         }
     else
@@ -36,5 +41,25 @@
             return $true
         }
     }
+else
+    {
+    if ($null -ne $comp)
+        {
+        switch($comp)
+            {
+                {$_.SideIndicator -eq "<="} {Write-Verbose "Source: $($_.InputObject) does not exist in $DestinationFolder";Copy-Item "$SourceFolder\$($_.InputObject)" -Destination $destinationFolder -Verbose}
+                {$_.SideIndicator -eq "=>"} {Write-Verbose "Destination: $($_.InputObject) does not exist in $SourceFolder";Remove-Item "$DestinationFolder\$($_.InputObject)" -Force -Verbose}
+            }
+        #Write-Verbose "Differences Found"
+        #return $false
+        }
+    else
+        {
+            Write-Verbose "No differences detected between $SourceFolder and $DestinationFolder"
+        #return $true
+        }
+    }
+
+}
 
 Export-ModuleMember *
